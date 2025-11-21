506539edaedffac410135abddad715bf706c7fc0
김예린 <yerin22333@gmail.com>
2025-11-21 12:33:22 +0900

    배열 데이터API 테스트 - 유저 추가

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+import * as authController from "../controller/auth.mjs";
 
 const router = express.Router();
 
 // 회원가입
+router.post("/signup", authController.signup);
 
 // 로그인
+router.post("/signin", authController.login);
 
 // 로그인 유지
 
